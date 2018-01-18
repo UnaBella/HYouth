ebc71aacf7242b94355d7066441660507f0670c7
una <tenglf@xiaoman.com>
2018-01-18 18:05:01 +0800

    index

---
+++ js/pubpart.js
@@ -0,0 +1,28 @@
+var pubpart = `<div class="in">
+			<div class="auto clearfix">
+				<div class="t fr">
+					<div class="unit">
+						<p class="p">沈阳公司：</p>
+						<div class="word">沈阳市青年大街23号希望大厦12F，后青春工业设计公司<br />
+电话：400-820-8989</div>						
+					</div>
+					<div class="unit">
+						<p class="p">大连公司：</p>
+						<div class="word">大连高新科技产业园区腾飞产业园二期<br />
+地下一层#-01-01室<br />
+电话：400-820-8989</div>						
+					</div>
+					<div class="unit">
+						<p class="p">蚌埠公司：</p>
+						<div class="word">蚌埠市解放路590号蚌埠人才市场5楼<br />
+电话：400-820-8989</div>						
+					</div>
+					<div class="icon">
+						<i class="icon1"></i>
+						<i class="icon2"></i>
+						<i class="icon3"></i>
+					</div>
+				</div>
+			</div>
+		</div>`
+			$('.pubpart').append(pubpart);
